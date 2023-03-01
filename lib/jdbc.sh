eeeeef7dee4d0bf90ec57dbb2871446e9d9b0f81
Emeric Martineau <11473190+emeric-martineau@users.noreply.github.com>
2023-03-01 16:48:47 +0100

    Fix download and copy

--- lib/jdbc.sh
+++ lib/jdbc.sh
@@ -3,5 +3,14 @@
 install_jdbc() {
   # Buildpack is run with -u (detecting uninitialize variables)
   # jvm-common buildpack doesn't work with it.
-  curl --retry 3 --location --output "/app/graviteeio-apim-rest-api/plugins/ext/repository-jdbc/postgresql-42.5.4.jar" "https://jdbc.postgresql.org/download/postgresql-42.5.4.jar"
+  local install_dir="$1"
+  local cache_dir="$2"
+  local jdbc_driver="postgresql-42.5.4.jar"
+  local cache_jdbc_driver="${cache_dir}/${jdbc_driver}"
+
+  if [ ! -f "${cache_jdbc_driver}" ]; then
+    curl --retry 3 --location --output "${cache_jdbc_driver}" "https://jdbc.postgresql.org/download/${jdbc_driver}"
+  fi
+
+  cp "${cache_jdbc_driver}" "${install_dir}/graviteeio-apim-rest-api/plugins/ext/repository-jdbc/${jdbc_driver}"
 }
