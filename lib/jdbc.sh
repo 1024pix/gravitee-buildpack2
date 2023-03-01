49ff90f042a41831db9f5415d74306d29382e0cf
Emeric Martineau <11473190+emeric-martineau@users.noreply.github.com>
2023-03-01 16:27:11 +0100

    fix download

--- lib/jdbc.sh
+++ lib/jdbc.sh
@@ -3,9 +3,5 @@
 install_jdbc() {
   # Buildpack is run with -u (detecting uninitialize variables)
   # jvm-common buildpack doesn't work with it.
-  set +u
-
-  curl --retry 3 --silent --location --output https://jdbc.postgresql.org/download/postgresql-42.5.4.jar /app/graviteeio-apim-rest-api/plugins/ext/repository-jdbc 
-
-  set -u
+  curl --retry 3 --location --output "/app/graviteeio-apim-rest-api/plugins/ext/repository-jdbc/postgresql-42.5.4.jar" "https://jdbc.postgresql.org/download/postgresql-42.5.4.jar"
 }
